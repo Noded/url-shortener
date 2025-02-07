16b9254d09fc6b2b7e65c5f9caf140f0f83829d3
Noded <owndyue@gmail.com>
2025-02-07 20:27:56 +0500

    add a shorting script

--- internal/shorten/shorten.go
+++ internal/shorten/shorten.go
@@ -1,1 +1,12 @@
-package shorten
+package main
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+)
+
+func ShortingURL(url string) string {
+	hash := md5.Sum([]byte(url))                          // MD5 hash
+	encoded := base64.URLEncoding.EncodeToString(hash[:]) // coding into base64
+	return encoded[:6]                                    // returning first six symbols
+}
